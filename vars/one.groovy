9b03b848a11fa41dc2491952c28fdd57a548de5a
gaddamchandrakanth <gaddamchandrakanth1995@gmail.com>
2025-04-11 18:30:47 +0530

    Update one.groovy

--- vars/one.groovy
+++ vars/one.groovy
@@ -1,18 +1,18 @@
-def
+def(a,b)
 {
   echo "The sum of the ${a} and ${b} is ${a+b}
 }
 
-def
+def(a,b)
 {
   echo "The subraction of the ${a} and ${b} is ${a-b}
 }
 
-def
+def(a,b)
 {
   echo " the product of the ${a} and ${b} is ${a*b}
 }
 
-def
+def(a,b)
 {
   echo " The divison of the ${a} amnd ${b} is ${a/b}

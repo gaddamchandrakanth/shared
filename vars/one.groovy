4dc9ec5f5724882f28d9f3084e9616e4b35dd07f
gaddamchandrakanth <gaddamchandrakanth1995@gmail.com>
2025-04-11 18:57:16 +0530

    Update one.groovy

--- vars/one.groovy
+++ vars/one.groovy
@@ -1,10 +1,9 @@
 def sum(a,b)
 {
-  echo "the sum of ${a} and ${b} is {a+b}"
+  echo "the sum of ${a} and ${b} is ${a+b}
 }
 
-def sub(a,b)
+def mul(a,b)
 {
-  echo "the subtraction of ${a} and ${b} is ${a-b}"
+  echo "the multiply of ${a} and ${b} is ${a/b}"
 }
-

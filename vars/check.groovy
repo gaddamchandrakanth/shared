a572f0fb20b7323ca01163be4d424b64af70e224
gaddamchandrakanth <gaddamchandrakanth1995@gmail.com>
2025-04-14 16:50:43 +0530

    Update check.groovy

--- vars/check.groovy
+++ vars/check.groovy
@@ -6,4 +6,12 @@ def gitBuild()
 {
   sh 'mvn package'
 }
+def gitdeploy(jobname,ip,context)
+{
+  sh "scp /var/lib/jenkins/workspace/declarativepipeline/webapp/target/webapp.war ubuntu@172.31.47.202:/var/lib/tomcat10/webapps/check.war"
+}
+def gittesting(repo)
+{
+    git "https://github.com/IntelliqDevops/${repo}.git" 
+}
 

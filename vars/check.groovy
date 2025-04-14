9aab05b8eb9c43a18426e0644c0469618f8543b8
gaddamchandrakanth <gaddamchandrakanth1995@gmail.com>
2025-04-14 17:10:44 +0530

    Update check.groovy

--- vars/check.groovy
+++ vars/check.groovy
@@ -14,4 +14,9 @@ def gittesting(repo)
 {
     git "https://github.com/IntelliqDevops/${repo}.git" 
 }
+def gitdelivery(jobname,ip,context)
+{
+    sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+}
+
 
